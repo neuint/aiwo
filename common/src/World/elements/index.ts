acf16280ff46841394ff7aeb1c786527fa19e130
Pavlo Gusar <p.gusar@yahoo.com>
2023-03-06 12:21:49 +0100

    build client

--- common/src/World/elements/index.ts
+++ common/src/World/elements/index.ts
@@ -12,7 +12,7 @@ export const setNextId = (id: number): void => {
 export const elementFactory = (params: BaseParamsType, id?: 'new' | 'skip' | 'left'): IElement | undefined => {
   if ((params as BoxParamsType).width && (params as BoxParamsType).height) {
     let elementId = nextId;
-    if (!id || id === 'new') setNextId(nextId = 1);
+    if (!id || id === 'new') setNextId(nextId + 1);
     if (id === 'skip') elementId = 0;
     if (id === 'left') elementId = params.id || 0;
     return new Box(elementId, params as BoxParamsType);
